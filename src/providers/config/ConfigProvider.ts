2f8b3d0b509e50bc9a4c9684f5fb01c8d77b7078
Alex Hicks <alex@alexhicks.net>
2018-08-28 13:17:59 -0400

    Use .env instead of ENV.env

--- src/providers/config/ConfigProvider.ts
+++ src/providers/config/ConfigProvider.ts
@@ -1,5 +1,4 @@
 import * as dotenv from "dotenv";
-import * as fs from "fs";
 import { IConfigurationKey } from "./IConfigurationKey";
 
 const REQUIRED_KEYS: IConfigurationKey[] = ["DB_TYPE"];
@@ -7,8 +6,8 @@ const REQUIRED_KEYS: IConfigurationKey[] = ["DB_TYPE"];
 export class ConfigProvider {
     private readonly config: { [key in IConfigurationKey]: string };
     constructor() {
-        const env = process.env.NODE_ENV || "dev";
-        this.config = dotenv.parse(fs.readFileSync(`${env}.env`)) as any;
+        dotenv.config();
+        this.config = process.env as any;
         const missingKeys = REQUIRED_KEYS.filter(k => !(k in this.config));
         if (missingKeys.length > 0) {
             throw new Error("Missing required configuration keys: " + missingKeys.join(", "));
