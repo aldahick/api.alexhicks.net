e020ceefbfe21b555ec9b6ecc8275cab5359190a
Alex Hicks <alex@alexhicks.net>
2018-08-28 12:40:38 -0400

    Fix TypeORM connection logic

--- src/providers/database/DatabaseProvider.ts
+++ src/providers/database/DatabaseProvider.ts
@@ -16,11 +16,11 @@ export const DatabaseProvider: nest.Provider = {
             connection = await orm.createConnection({
                 type: config.get("DB_TYPE") as any,
                 host: config.get("DB_HOSTNAME"),
-                port: config.get("DB_PORT"),
+                port: Number(config.get("DB_PORT")),
                 username: config.get("DB_USERNAME"),
                 password: config.get("DB_PASSWORD"),
                 database: config.get("DB_DATABASE"),
-                entities: Object.values(models),
+                entities: Object.values(models).filter(m => typeof(m) === "function") as any[],
                 synchronize: true,
                 namingStrategy: new NamingStrategy()
             });
